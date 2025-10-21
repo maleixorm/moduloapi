c685b0380577171f65a76ea1d9bdee8a99ca6dfb
Marcos Aleixo <maknytz@gmail.com>
2025-10-21 16:26:48 -0300

    Criando os endpoints get e put

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using moduloapi.Context;
@@ -26,6 +27,30 @@ namespace moduloapi.Controllers
             _context.Add(contato);
             _context.SaveChanges();
             return Ok(contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null) return NotFound();
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if (contatoBanco == null) return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
         }
     }
 }
